d6852d112381c89aa3966a22a8d5fb512df2dd2d
Saumil <sapatel2727@gmail.com>
2020-09-14 12:01:39 +1000

    Remove Gradle Properties

--- app/src/main/java/com/example/runtracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/runtracker/ui/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.runtracker.ui
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,7 +7,6 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.runtracker.R
 import com.example.runtracker.database.RunDao
-import com.example.runtracker.other.Constants.SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
@@ -20,8 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(nav_host_fragment.findNavController())
@@ -35,16 +31,8 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-    }
 
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-        navigateToTrackingFragmentIfNeeded(intent)
-    }
 
-    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?){
-        if(intent?.action == SHOW_TRACKING_FRAGMENT){
-            nav_host_fragment.findNavController().navigate(R.id.action_global_trackingFragment)
-        }
+
     }
 }
